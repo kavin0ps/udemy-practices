a638779c8b147577735fbf8e9e0b1b3c46103be2
Kavin0ps <kavin2ps@gmail.com>
2025-12-29 19:48:55 +0530

    Fix BMI calculator logic

--- bmi/bmi.js
+++ bmi/bmi.js
@@ -2,10 +2,27 @@
    var kg=prompt("Enter The Weight(kgs)");
    var m=prompt("Enter The Height(m)");
    console.log(calcbmi(kg,m));
-   alert("your bmi is " + calcbmi(kg,m));
-   
+   console.log(calcbmi(kg,m));
+   var bmiv= calcbmi(kg,m);
    function calcbmi(kg,m)
    {
      var bmi=kg/(m*m);
+     bmi=Math.floor(bmi);
       return bmi;
    }
+   alert(bmiCalculator(bmiv));
+   function bmiCalculator(bmiv) {
+   
+   
+   if (bmiv<18.5){
+    return "Your BMI is " + bmiv + ", so you are underweight.";
+   }
+   
+   if (bmiv>18.5&&bmiv<24.9){
+    return "Your BMI is " + bmiv + ", so you have a normal weight.";
+   }
+   
+   if (bmiv>24.9){
+    return "Your BMI is " + bmiv + ", so you are overweight.";
+   }
+}
